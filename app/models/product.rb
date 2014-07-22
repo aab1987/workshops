10f9a52fb8c927842fdb55abd36cc4a7f693b55f
Agata Brodniewska <aab1987@wp.pl>
2014-07-22 20:54:11 +0200

    Solved average rating issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ActiveRecord::Base
+validates_presence_of :title
+validates_presence_of :description
+validates_presence_of :price
+
+def average_rating
+  reviews.map(&:rating).reduce(:+)/reviews.count.to_f
+end
+
+validates_format_of :price, :with => /\d+\.\d{1,2}/
+
   belongs_to :category
   belongs_to :user
   has_many :reviews
